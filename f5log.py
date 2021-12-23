50a6c9c1fe4e4e1c0221ae5b97c5ac86bd1678a8
James Deucker <me@bitwisecook.org>
2021-12-23 14:50:02 +0000

    add some basic colourisation, refactor some names, fix some log bugs, parse more logs

--- f5log.py
+++ f5log.py
@@ -1,14 +1,17 @@
 __name__ = "f5log"
 __author__ = "James Deucker <me@bitwisecook.org>"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from datetime import datetime, timedelta
 from functools import partial
 from ipaddress import ip_address
 import re
+import traceback
 from typing import Any, Dict, Optional
 
-from visidata import Path, VisiData, Sheet, date, ColumnAttr, vd
+from visidata import Path, VisiData, Sheet, date, ColumnAttr, vd, theme
+from visidata.column import Column
+from visidata.sheets import CellColorizer, RowColorizer
 
 hexint = partial(int, base=16)
 delta_t = partial(int, base=10)
@@ -19,6 +22,14 @@ vd.addType(
     delta_t,
     icon="⇥",
     formatter=lambda fmt, delta: str(timedelta(seconds=delta)),
+)
+theme("color_f5log_mon_up", "green", "color of f5log monitor status up")
+theme("color_f5log_mon_down", "red", "color of f5log monitor status down")
+theme("color_f5log_mon_unknown", "blue", "color of f5log monitor status unknown")
+theme("color_f5log_mon_checking", "magenta", "color of monitor status checking")
+theme("color_f5log_mon_disabled", "black", "color of monitor status disabled")
+theme(
+    "color_f5log_logid_warning", "red", "color of something urgent to pay attention to"
 )
 
 
@@ -80,7 +91,7 @@ class F5LogSheet(Sheet):
         r"(?:/Common/(?P<customer_id>[a-z0-9_]{1,23})-(?:\1-)?(?P<proxy_id>\d+)(?:_(?P<service_id>\d+)\.app/)?)"
     )
     re_ltm_pool_mon_status_msg = re.compile(
-        r"^Pool\s(?P<poolobj>\S+)\smember\s(?P<poolmemberobj>\S+)\smonitor\sstatus\s(?P<newstatus>\w+)\.\s\[\s(?P<monitorobj>\S+):\s(?P<monitorstatus>\w+)(?:;\slast\serror:\s\S*\s?(?P<lasterr>.*))?\s]\s+\[\swas\s(?P<prevstatus>\w+)\sfor\s(?P<durationhr>\d+)hrs?:(?P<durationmin>\d+)mins?:(?P<durationsec>\d+)sec\s\]$"
+        r"^(Pool|Node)\s(?P<poolobj>\S+)\s(member|address)\s(?P<poolmemberobj>\S+)\smonitor\sstatus\s(?P<newstatus>.+)\.\s\[\s((?P<monitorobj>\S+):\s(?P<monitorstatus>\w+)(?:;\slast\serror:\s\S*\s?(?P<lasterr>.*))?)?\s]\s+\[\swas\s(?P<prevstatus>.+)\sfor\s(?P<durationhr>\d+)hrs?:(?P<durationmin>\d+)mins?:(?P<durationsec>\d+)sec\s\]$"
     )
     re_ltm_ip_msg = re.compile(
         r"(?:.*?)(?P<ip1>\d+\.\d+\.\d+\.\d+)(?:[:.](?P<port1>\d+))?(?:(?:\s->\s|:)(?P<ip2>\d+\.\d+\.\d+\.\d+)(?:[:.](?P<port2>\d+))?)?(?:\smonitor\sstatus\s(?P<mon_status>\w+)\.\s\[[^]]+\]\s+\[\swas\s(?P<prev_status>\w+)\sfor\s((?P<durationhr>\d+)hrs?:(?P<durationmin>\d+)mins?:(?P<durationsec>\d+)secs?)\s\]|\.?(?:.*))"
@@ -94,9 +105,56 @@ class F5LogSheet(Sheet):
     re_gtm_monitor = re.compile(
         r"^(?:SNMP_TRAP:\s)?(?P<objtype>VS|Pool|Monitor|Wide\sIP|Server)\s(?P<object>\S+)\s(?:member\s(?P<pool_member>\S+)\s)?(?:\(ip(?::port)?=(?P<ipport>[^\)]+)\)\s)?(?:\(Server\s(?P<server>[^\)]+)\)\s)?(?:state\schange\s)?(?P<prev_status>\w+)\s-->\s(?P<new_status>\w+)(?:(?:\s\(\s?)(?P<msg>(?:(?P<type>\w+)\s(?P<monitor_object>\S+)\s:\s)?state:\s(?P<state>\S+)|.*)\))?"
     )
+    re_ltm_poolnode_abled = re.compile(
+        r"^(?:Pool|Node)\s(?P<object>\S+)\s(?:address|member)\s(?P<member>\S+)\ssession\sstatus\s(?P<status>.+)\.$"
+    )
+
+    f5log_mon_colors = {
+        ("monitor_status", "down"): "color_f5log_mon_down",
+        ("monitor_status", "up"): "color_f5log_mon_up",
+        ("monitor_status", "enabled"): "color_f5log_mon_up",
+        ("monitor_status", "forced disabled"): "color_f5log_mon_disabled",
+        ("monitor_status", "node disabled"): "color_f5log_mon_disabled",
+        ("new_status", "available"): "color_f5log_mon_up",
+        ("new_status", "unavailable"): "color_f5log_mon_down",
+        ("new_status", "up"): "color_f5log_mon_up",
+        ("new_status", "down"): "color_f5log_mon_down",
+        ("new_status", "green"): "color_f5log_mon_up",
+        ("new_status", "red"): "color_f5log_mon_down",
+        ("new_status", "now has available members"): "color_f5log_mon_up",
+        ("new_status", "no members available"): "color_f5log_mon_down",
+        ("new_status", "blue"): "color_f5log_mon_unknown",
+        ("new_status", "checking"): "color_f5log_mon_checking",
+        ("new_status", "unchecked"): "color_f5log_mon_unknown",
+        ("new_status", "node down"): "color_f5log_mon_disabled",
+        ("prev_status", "available"): "color_f5log_mon_up",
+        ("prev_status", "unavailable"): "color_f5log_mon_down",
+        ("prev_status", "up"): "color_f5log_mon_up",
+        ("prev_status", "down"): "color_f5log_mon_down",
+        ("prev_status", "green"): "color_f5log_mon_up",
+        ("prev_status", "red"): "color_f5log_mon_down",
+        ("prev_status", "now has available members"): "color_f5log_mon_up",
+        ("prev_status", "no members available"): "color_f5log_mon_down",
+        ("prev_status", "blue"): "color_f5log_mon_unknown",
+        ("prev_status", "checking"): "color_f5log_mon_checking",
+        ("prev_status", "unchecked"): "color_f5log_mon_unknown",
+        ("prev_status", "node down"): "color_f5log_mon_disabled",
+    }
+
+    def colorizeMonitors(sheet, col: Column, row: F5LogRow, value):
+        if row is None or col is None:
+            return None
+        return sheet.f5log_mon_colors.get((col.name, value.value), None)
+
+    f5log_warn_logid = {"01190004": "color_f5log_logid_warning"}
+
+    def colorizeWarnings(sheet, col: Column, row: F5LogRow, value):
+        if row is None or col is None:
+            return None
+        return sheet.f5log_warn_logid.get(row.logid1, None)
 
     @staticmethod
-    def split_audit_01420002(msg):
+    def split_audit_bigip_tmsh_audit(msg):
         # skip 'AUDIT - ' at the start of the line
         e = msg[8:].split("=", maxsplit=6)
 
@@ -104,7 +162,7 @@ class F5LogSheet(Sheet):
             yield {ee[ee.rfind(" ") + 1 :]: ne[: ne.rfind(" ")]}
 
     @staticmethod
-    def split_audit_014F0005(msg):
+    def split_audit_scriptd_run_script(msg):
         # skip 'AUDIT - ' at the start of the line
         e = msg[8:].split("=")
 
@@ -112,7 +170,7 @@ class F5LogSheet(Sheet):
             yield {ee[ee.rfind(" ") + 1 :]: ne[: ne.rfind(" ")].strip('"')}
 
     @staticmethod
-    def split_audit_01070417(msg):
+    def split_audit_mcpd_mcp_error(msg):
         # skip 'AUDIT - ' at the start of the line
         # skip the status at the end of the line
         if msg[msg.rfind("[Status=") :].startswith("[Status="):
@@ -163,12 +221,15 @@ class F5LogSheet(Sheet):
         dst = m.get("poolmemberobj")
         if dst:
             dst = dst.split("/")[-1]
-            if len(dst.split(":")) == 2:
+            if "." in dst and len(dst.split(":")) == 2:
                 # ipv4
                 dst_ip, dst_port = dst.split(":")
+            elif "." in dst and len(dst.split(":")) == 1:
+                # ipv4
+                dst_ip, dst_port = dst, None
             else:
                 # ipv6
-                dst_ip, dst_port = dst.split(".")
+                dst_ip, dst_port = dst.rsplit(":", maxsplit=1)
             try:
                 # see if it's an IP and if so parse it
                 dst_ip = ip_address(dst_ip)
@@ -177,7 +238,7 @@ class F5LogSheet(Sheet):
             try:
                 # see if it's a port number and if so parse it
                 dst_port = int(dst_port)
-            except ValueError:
+            except (ValueError, TypeError):
                 dst_port = None
         else:
             dst_ip, dst_port = None, None
@@ -192,6 +253,18 @@ class F5LogSheet(Sheet):
             "duration_s": duration,
             "dst_ip": dst_ip,
             "dst_port": dst_port,
+        }
+
+    @staticmethod
+    def split_ltm_poolnode_mon_abled(msg):
+        m = F5LogSheet.re_ltm_poolnode_abled.match(msg)
+        if m is None:
+            return
+        m = m.groupdict()
+        yield {
+            "object": m.get("object"),
+            "member": m.get("member"),
+            "monitor_status": m.get("status"),
         }
 
     @staticmethod
@@ -406,16 +479,19 @@ class F5LogSheet(Sheet):
             "dst_port": int(dst_port) if dst_port else None,
             "prev_status": m[4].lower(),
             "new_status": m[6].lower(),
-            "src_gtm": ip_address(m[8]),
+            "src_gtm": m[8],
             "state": m[10][:-1].lower(),
         }
 
     splitters = {
         0x01010028: split_ltm_pool_has_no_avail_mem.__func__,
         0x01010221: split_ltm_pool_has_avail_mem.__func__,
-        0x01070417: split_audit_01070417.__func__,
+        0x01070417: split_audit_mcpd_mcp_error.__func__,
         0x01070638: split_ltm_pool_mon_status.__func__,
+        0x01070639: split_ltm_poolnode_mon_abled.__func__,
+        0x01070641: split_ltm_poolnode_mon_abled.__func__,
         0x01070727: split_ltm_pool_mon_status.__func__,
+        0x01070728: split_ltm_pool_mon_status.__func__,
         0x01071681: split_ltm_virtual_status.__func__,
         0x01071682: split_ltm_virtual_status.__func__,
         0x01071BA9: split_ltm_virtual_status.__func__,
@@ -447,10 +523,10 @@ class F5LogSheet(Sheet):
         0x01260026: split_ltm_shared_ciphers.__func__,
         0x01260008: split_ltm_connection_error.__func__,
         0x01260009: split_ltm_connection_error.__func__,
-        0x01420002: split_audit_01420002.__func__,
+        0x01420002: split_audit_bigip_tmsh_audit.__func__,
         0x01420007: split_ltm_cert_expiry.__func__,
         0x01420008: split_ltm_cert_expiry.__func__,
-        0x014F0005: split_audit_014F0005.__func__,
+        0x014F0005: split_audit_scriptd_run_script.__func__,
     }
 
     # these logs can have IDs we care about splitting but would be errors
@@ -474,6 +550,12 @@ class F5LogSheet(Sheet):
         "message",
         "object",
     }
+
+    # precedence, coloropt, func
+    colorizers = [
+        CellColorizer(100, None, colorizeMonitors),
+        RowColorizer(101, None, colorizeWarnings),
+    ]
 
     def iterload(self):
         self.rows = []  # rowdef: [F5LogRow]
@@ -516,7 +598,14 @@ class F5LogSheet(Sheet):
                     for entry in F5LogSheet.splitters[logid1](m.get("message")):
                         kv.update(entry)
                 except (IndexError, ValueError) as exc:
-                    kv = {"PARSE_ERROR": str(exc)}
+                    kv = {
+                        "rawmsg": line,
+                        "PARSE_ERROR": "\n".join(
+                            traceback.format_exception(
+                                etype=type(exc), value=exc, tb=exc.__traceback__
+                            )
+                        ),
+                    }
                 for k, v in kv.items():
                     if k not in self.extra_cols:
                         F5LogSheet.addColumn(self, ColumnAttr(k))
